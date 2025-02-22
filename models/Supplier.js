f97cc84eaa149a38255161aece2a2ea1b744eec9
Jahangir Alom <alomj257@gmail.com>
2025-02-22 18:00:57 +0530

    purchaseDate

--- models/Supplier.js
+++ models/Supplier.js
@@ -1,3 +1,61 @@
+// const mongoose = require('mongoose');
+
+// const SupplierSchema = new mongoose.Schema({
+//     supplierName: {
+//         type: String,
+//         required: true
+//     },
+//     supplierAddress: {
+//         type: String,
+//     },
+//     email: {
+//         type: String,
+//     },
+//     gst: {
+//         type: String,
+//     },
+//     contactDetails: {
+//         type: String,
+//         required: true
+//     },
+//     items: [
+//         {
+//             itemName: {
+//                 type: String,
+//                 required: true,
+//                 lowercase: true
+//             },
+//             unit: {
+//                 type: String,
+//                 required: true
+//             },
+//             itemQuantity: {
+//                 type: Number,
+//                 required: true
+//             },
+//             pricePerItem: {
+//                 type: Number,
+//                 required: true
+//             },
+//             totalPrice: {
+//                 type: Number,
+//                 required: true,
+//                 default: function() {
+//                     return this.itemQuantity * this.pricePerItem;
+//                 }
+//             }
+//         }
+//     ]
+// },{ timestamps: true });
+
+// // Method to calculate total price of all items in a supplier
+// SupplierSchema.methods.calculateTotalPrice = function() {
+//     return this.items.reduce((sum, item) => sum + item.totalPrice, 0);
+// };
+
+// module.exports = mongoose.model('Supplier', SupplierSchema);
+
+
 const mongoose = require('mongoose');
 
 const SupplierSchema = new mongoose.Schema({
@@ -45,10 +103,13 @@ const SupplierSchema = new mongoose.Schema({
                 }
             }
         }
-    ]
+    ],
+    purchaseDate: {
+        type: Date,
+        required: true
+    }
 },{ timestamps: true });
 
-// Method to calculate total price of all items in a supplier
 SupplierSchema.methods.calculateTotalPrice = function() {
     return this.items.reduce((sum, item) => sum + item.totalPrice, 0);
 };
